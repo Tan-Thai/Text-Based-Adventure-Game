418d017e3c0b7c27d3d75e7884e6d28df9f2565f
HebePython <henrik.bergman.edu@gmail.com>
2024-10-29 10:38:12 +0100

    update slowprint

--- src/Global/Slowprint.java
+++ src/Global/Slowprint.java
@@ -1,13 +1,13 @@
 package Global;
 public class Slowprint {
 
-    private int delay = 40; // Default delay
+    private static int delay = 40; // Default delay
 
     public void setDelay(int delay) {
         this.delay = delay;
     }
 
-    public void slowPrint(String text) {
+    public static void sp(String text) {
         for (int i = 0; i < text.length(); i++) {
             char currentChar = text.charAt(i);
             System.out.print(currentChar);
@@ -22,12 +22,12 @@ public class Slowprint {
         System.out.println();
     }
 
-    public void slowPrint(int number) {
-        slowPrint(Integer.toString(number)); // Convert int to String
+    public void sp(int number) {
+        sp(Integer.toString(number)); // Convert int to String
     }
 
-    public void slowPrint(Object obj) {
+    public void sp(Object obj) {
         // Handle any object by converting it to a String
-        slowPrint(obj.toString());
+        sp(obj.toString());
     }
 }
