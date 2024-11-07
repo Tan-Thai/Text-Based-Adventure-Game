66fd21254aec2d28d3571db03260596a3b947ad9
WilleVirtanen <virtanen.wille@gmail.com>
2024-11-07 11:37:05 +0100

    Create EndOfGameScreen.java

---
+++ src/Global/EndOfGameScreen.java
@@ -0,0 +1,66 @@
+package Global;
+
+import java.util.Scanner;
+
+public class EndOfGameScreen {
+    public static boolean gameOver = true;
+    public static boolean death = true;
+    public static boolean gameOnGoing = true;
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void gameOverCheck() {
+        if (gameOver == true) {
+            if (death == false) {
+                victoryScreen();
+            } else {
+                gameOverScreen();
+            }
+        } else {
+        }
+
+    }
+
+    public static void victoryScreen() {
+        System.out.println("Victory!");
+        System.out.println("Would you like to play again?(Y/N?)");
+        if(Utility.checkYesOrNo(sc))
+        {
+            gameOnGoing=true;
+        }
+        else
+        {
+            gameOnGoing=false;
+        }
+
+    }
+
+    public static void gameOverScreen() {
+        System.out.println("Game Over!");
+        System.out.println("Would you like to play again?(Y/N?)");
+        if(Utility.checkYesOrNo(sc))
+        {
+            gameOnGoing=true;
+        }
+        else
+        {
+            gameOnGoing=false;
+        }
+
+    }
+
+    public static void endOfgame() {
+        EndOfGameScreen.gameOverCheck();
+
+    }
+
+    //just for testing the code
+    public static void main(String[] args) {
+        while (gameOnGoing) {
+            EndOfGameScreen.endOfgame();
+        }
+    }
+
+}
+
+}
