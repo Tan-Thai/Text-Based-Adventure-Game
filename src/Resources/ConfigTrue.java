6cf9b0729a1b8d89d80fd2f4c73a7ee84a6bc675
HebePython <50890910+HebePython@users.noreply.github.com>
2024-11-14 12:27:24 +0100

    Trying to fix invisible config file.

---
+++ src/Resources/ConfigTrue.java
@@ -0,0 +1,10 @@
+package Resources;
+
+public class ConfigTrue {
+    public static final int PC_STARTING_LEVEL = 1;
+    public static final int PC_MAX_LEVEL = 5;
+    public static final int PC_RETIREMENT_LEVEL = 3;
+    public static final int PC_EXPERIENCE_PER_LEVEL = 30;
+    public static final int EXPERIENCE_PER_ENCOUNTER = 20;
+    public static final int ZONE_CLEAR_THRESHOLD = 10;
+}
