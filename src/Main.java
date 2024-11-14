c11a98c03e732aac5642def02f7c3e8e075a3f81
Henrik B <50890910+HebePython@users.noreply.github.com>
2024-11-14 17:06:08 +0100

    Tavern-retirement + menufix (#31)
    * Changed while loop to startmenu to check gamestate.
    
    fixed tavern menu to not be recursive
    
    * Update Main.java
    
    Minor indentation removal
    
    ---------
    
    Co-authored-by: Tan-Thai <134406558+Tan-Thai@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -33,9 +33,8 @@ public class Main {
     }
 
     public static void startMenu(Game game, Scanner sc) {
+        while (GameStateManager.getInstance().getCurrentState() == GameState.RUNNING) {
         Utility.clearConsole();
-
-        while (true) {
         System.out.println("Welcome to the game!" +
                 "\n1. Start Game" +
                 "\n2. How to Play" +
@@ -57,3 +56,4 @@ public class Main {
         }
     }
 }
+
